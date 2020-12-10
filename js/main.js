10cac2e9df2ece58244643f7cea861fd34b4f804
Сергій Зубрін <absminoga@gmail.com>
2020-12-10 04:19:50 +0200

    Mac commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+// 	var videobackground = new $.backgroundVideo('$(body)', {
+// 	  "width": 100%,
+// 	  "height": calc(100px + 7.86vw),
+// 	  "path": "video/",
+// 	  "filename": "Pexels",
+// 	  "types": ["mp4","ogg","webm"],
+// 	  "preload": true,
+// 	  "autoplay": true,
+// 	  "loop": true
+// 	});
+//   });
